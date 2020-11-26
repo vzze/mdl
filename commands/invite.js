2e03f13dab188b41a044e9cd9e64569960e8e338
vzze <lolo32778@gmail.com>
2020-11-26 11:58:18 +0200

    added invite command

---
+++ commands/invite.js
@@ -0,0 +1,16 @@
+const Discord = require(`discord.js`);
+const config = require('../config/config.json')
+const pref = config.prefix;
+
+module.exports = {
+    name: 'invite',
+    description: 'Sends a message with a hyper link to invite the bot.',
+    usage: `${pref}invite`,
+    cooldown: 1,
+    async execute(client, message, args, users, ranks, Canvas, lvls) {
+        const l4embed = new Discord.MessageEmbed()
+            .setColor('#ad26d1')
+            .setDescription(`**To Invite me to your server [click here](https://discord.com/api/oauth2/authorize?client_id=776551374380204033&permissions=268520448&scope=bot)**`)
+        message.channel.send(l4embed);
+    }
+}
