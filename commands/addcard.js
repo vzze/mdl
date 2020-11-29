23b9148f49a05c1a3522ea8951528f1c3e61920e
vzze <lolo32778@gmail.com>
2020-11-29 14:48:09 +0200

    added nsfw check

--- commands/addcard.js
+++ commands/addcard.js
@@ -1,11 +1,12 @@
 const Discord = require(`discord.js`);
 const pref = require('../config/config.json')
+const deepai = require('deepai');
 
 module.exports = {
     name: 'addcard',
     description: "Adds a custom rank card for users above level 20. \n Suggested resolution is 934x282",
     usage: `\`${pref.prefix}addcard\` <Imgur link> \n \`${pref.prefix}addcard\` <Imgur link>`,
-    cooldown: 3,
+    cooldown: 5,
     async execute(client, message, args, users, ranks, Canvas, lvls) {
         const us = await users.findOne({ user_id: message.author.id });
         if(us.level>=20) {
@@ -13,6 +14,18 @@ module.exports = {
                 if(args[0].indexOf("imgur")!=-1) {
                     if(args[0].indexOf(".png")!=-1 || args[0].indexOf(".jpg")!=-1 || args[0].indexOf(".jpeg")!=-1) {
                         var ok = 1;
+                        deepai.setApiKey('3735ad30-254c-40d8-9b50-ce0cfb3f4707');
+
+                        var resp = await deepai.callStandardApi("nsfw-detector", {
+                                image: `${args[0]}`,
+                        });
+                        let nsfwcheck = resp.output.nsfw_score;
+                        if(nsfwcheck >= 0.6) {
+                            const nsfwdembed = new Discord.MessageEmbed()
+                                .setColor('#dd4545')
+                                .setDescription(`**Image is not safe for work.**`)
+                            return message.channel.send(nsfwdembed);
+                        }
                         try {
                             const u = await users.findOne({ user_id: message.author.id });
                             await u.updateOne({ rankcardlink: args[0] });
