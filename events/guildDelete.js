44296cd6fa1c27c006c4045e36572a75facde99b
vzze <74437706+vzze@users.noreply.github.com>
2020-12-03 15:11:41 +0200

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,7 +4,7 @@ const ranks = require('../data/ranks');
 client.on('guildDelete', async guild => {
     await ranks.deleteMany({guild_id: `${guild.id}`});
     setTimeout( () => {
-        client.user.setActivity(`.mhelp | discord.gg/mandem`, {
+        client.user.setActivity(`.mhelp | ${client.guilds.cache.size} guilds | discord.gg/mandem`, {
             type: "WATCHING",
         });
     }, 30000);
