f5e35832259d6833fa7e8f92de56c026810f871a
vzze <74437706+vzze@users.noreply.github.com>
2020-12-03 09:17:11 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,7 +3,7 @@ const ranks = require('../data/ranks');
 
 client.on('guildCreate', async guild => {
     setTimeout( () => {
-        client.user.setActivity(`discord.gg/mandem`, {
+        client.user.setActivity(`.mhelp | discord.gg/mandem`, {
             type: "WATCHING",
         });
     }, 30000);
