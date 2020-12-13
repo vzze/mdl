f17edb9a4bc14d48d9a7e7e9ff0c6646f110a548
vzze <74437706+vzze@users.noreply.github.com>
2020-12-13 19:13:39 +0200

    ready ev changes

--- events/ready.js
+++ events/ready.js
@@ -2,8 +2,8 @@ module.exports = (client) => {
     if(client.shard.ids[0] === 0) {
         console.log(`Logged in as ${client.user.tag}`);
         console.log(" ")
-        client.user.setActivity(`${client.guilds.cache.size} guilds | .mhelp`, {
-            type: "WATCHING",
-        });
     }
+    client.user.setActivity(`${client.guilds.cache.size} guilds | .mhelp`, {
+        type: "WATCHING",
+    });
 }
